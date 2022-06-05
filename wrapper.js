9120dade78add5880d41ea78b06df569face6651
Nanda <mariafsguedes@fac.pe.senac.br>
2022-06-05 15:45:28 -0300

    teste no wrapper

--- wrapper.js
+++ wrapper.js
@@ -2,5 +2,8 @@ const number1 = require('extenso')
 
 const number2 = require('numero-por-extenso');
 
+const number7 = require('num2words'); 
+
 console.log(number1(22))
-console.log(number2.porExtenso(33))
+console.log(number2.porExtenso(22))
+console.log(number7.numToWords(22))
