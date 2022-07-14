81c297606653a5ba18f7e1ff5ec2f6092594f52d
Nanda <mariafsguedes@fac.pe.senac.br>
2022-07-13 21:57:52 -0300

    tentando resolver o problema do indexof

--- recentes/projeto.js
+++ recentes/projeto.js
@@ -7,9 +7,10 @@ const nums = {
   'três' : 3
 }
 
-const exi = input.indexOf(Object.keys(nums)) //checa se o que foi digitado existe no obj mãe e retorna o index do valor correspondente;
-  if (exi == 0) 
-    console.log("Valor inválido!")
-return Object.values(nums)[exi] //pesquisa o indexof nos values e retorna ele
-}
+console.log(Object.keys(nums))}
+//const exi = input.indexOf(Object.keys(nums)) //checa se o que foi digitado existe no obj mãe e retorna o index do valor correspondente;
+ // if (exi == 0) 
+   // console.log("Valor inválido!")
+//return Object.values(nums)[exi] //pesquisa o indexof nos values e retorna ele
+//}
 module.exports = {toWord}
